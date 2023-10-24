e532b9611355874b25d5fac983759c89aa333d4b
João Netto <jovinettonetto@gmail.com>
2023-10-24 14:56:06 +0000

    concluido

--- Principal.java
+++ Principal.java
@@ -1,5 +1,11 @@
 public class Principal {
     public static void main(String[] args) {
-        
+        Carro carAro = new Carro(20);
+        Carro car = new Carro(20, "gol g1", 13000, "vermelho");
+        Carro car2 =  new Carro(28, "gol G1", 24000, "preto");
+    
+        System.out.println(car.getModelo());
+        System.out.println(car2.getModelo());
+        System.out.println(carAro.getAro());
     }
 }
